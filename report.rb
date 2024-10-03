28039fd197d3f304c0852fc5b21ce94aa78ee993
obikosato <obiko.sato@gmail.com>
2024-10-04 01:32:41 +0900

    fix rubocop warnings

--- report.rb
+++ report.rb
@@ -1,10 +1,10 @@
 require 'dotenv'
 
-Dotenv.load 
+Dotenv.load
 
-SECRET_KEY = ENV['SECRET_KEY']
-HOGE_VALUE = ENV['HOGE_VALUE']
-RUN_FROM = ENV['RUN_FROM']
+SECRET_KEY = ENV.fetch('SECRET_KEY', nil)
+HOGE_VALUE = ENV.fetch('HOGE_VALUE', nil)
+RUN_FROM = ENV.fetch('RUN_FROM', nil)
 
 puts "SECRET_KEY: #{SECRET_KEY}"
 puts "HOGE_VALUE: #{HOGE_VALUE}"
